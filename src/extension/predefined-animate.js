559b6e2f784dbd246574bb4be57fc8cb6e1a39e6
youLookLikeDelicious <1059366817@qq.com>
2020-10-09 21:11:23 +0800

    modify slideup and slidedown

--- src/extension/predefined-animate.js
+++ src/extension/predefined-animate.js
@@ -4,20 +4,28 @@ const autoConfig = {
     // 渐现
     // 只要不透明度小于1，就执行动画
     fadeIn: function (el) {
-        if(this.css(el, 'opacity') < 1){
-            return {"opacity": 1}
+        if (this.css(el, 'opacity') < 1) {
+            return {
+                "opacity": 1
+            }
         }
     },
     // 渐隐
     // 只要不透明度大于0就执行动画
     fadeOut: function (el) {
-        if(this.css(el, 'opacity')  > 0){
-            return {"opacity": 0}
+        if (this.css(el, 'opacity') > 0) {
+            return {
+                "opacity": 0
+            }
         }
     },
     // 自动渐隐渐现
     fadeToggle: function (el) {
-        return this.css(el, 'opacity') > 0 ? {"opacity": 0} : {"opacity": 1}
+        return this.css(el, 'opacity') > 0 ? {
+            "opacity": 0
+        } : {
+            "opacity": 1
+        }
     },
     // 向上滑动
     slideUp: function (el) {
@@ -25,13 +33,13 @@ const autoConfig = {
         let h = el.getAttribute('slideTo')
 
         // 如果元素没有slidTo属性，添加之，值为元素当前的计算高度
-        if( !h ){
-            h = this.css(el, 'height')
-            el.setAttribute( 'slideTo', h )
-        }
+        h = this.css(el, 'height')
+        el.setAttribute('slideTo', h)
 
-        if(h > 0){
-            return {height: '0px'};
+        if (h > 0) {
+            return {
+                height: '0px'
+            };
         }
     },
     // 向下滑动
@@ -42,13 +50,16 @@ const autoConfig = {
         if (this.getStyle(el, 'display', false) === 'none') {
             el.style.display = el.getAttribute('data-display') || 'block'
         }
-        // 没有slideTo Attribute 添加之
+
         if (!h) {
             h = this.css(el, 'height');
-            el.setAttribute('slideTo', h);
-            el.style.height = '0px';
         }
-        return {height: h + 'px'}
+
+        el.style.height = '0px';
+
+        return {
+            height: h + 'px'
+        }
     },
     /**
      * 自动上下滑动
@@ -57,13 +68,12 @@ const autoConfig = {
      * @param {DOM} el 
      */
     slideToggle: function (el) {
-        let  curHeight = this.css(el, 'height'), // 获取元素当前的高度
+        let curHeight = this.css(el, 'height'), // 获取元素当前的高度
             display = this.getStyle(el, 'display', false)
 
         if (curHeight === 0 || display === 'none') {
             return this.slideDown(el);
-        }
-        else if (curHeight > 0) {
+        } else if (curHeight > 0) {
             return this.slideUp(el);
         }
     }
